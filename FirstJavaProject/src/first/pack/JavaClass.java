3320718abb56361a5fbf1ca4548103cf12410277
nomadlife <joonwoo3816@gmail.com>
2017-07-15 23:27:45 +0900

    1H

---
+++ FirstJavaProject/src/first/pack/JavaClass.java
@@ -0,0 +1,11 @@
+package first.pack;
+
+public class JavaClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+
+	}
+
+}
---
+++ JavaClass.java
@@ -0,0 +1,7 @@
+//package first.pack;
+
+public class JavaClass{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
