c3acc628a8afad94ea5564298f088ffbb6dfe8cf
nomadlife <joonwoo3816@gmail.com>
2017-07-17 23:00:12 +0900

    3H

--- FirstJavaProject/src/first/pack/PrintPrimativeTypes.java
+++ FirstJavaProject/src/first/pack/PrintPrimativeTypes.java
@@ -6,10 +6,12 @@ public class PrintPrimativeTypes {
 		// TODO Auto-generated method stub
 		int a = 2;
 		double b = 3.14;
+		int c=4;
+		double d = c*3.14;
 		System.out.println("1.Old Value of b="+b);
 		b=a;
 		System.out.println("2.New value of b="+b);
-
+		System.out.println("3.New value of d="+b);
 	}
 
 }
